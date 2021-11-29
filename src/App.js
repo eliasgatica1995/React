3128bcf202d749eef82a61368270c116b02d25f0
Elias <gonzaelias95@gmail.com>
2021-11-29 02:01:09 -0300

    Agregado de gif

--- src/App.js
+++ src/App.js
@@ -33,9 +33,7 @@ function App() {
                 <Route path="*" element={ <Navigate to="/"/> }/>
     
             </Routes>
-            
-                <FyH/>
-              
+           
                   
         </Container>
           
